cb7cc111d6c1283e6b225b12b3696c75d60db6b5
bunhor-deb <bunhor0303@gmail.com>
2023-08-16 20:37:59 +0700

    working on section 2

---
+++ springsecuritysection2/src/main/java/com/eazybytes/controllers/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.eazybytes.springsecuritybasic.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring security application with security.";
+    }
+
+}
